40a4f6f9715c9eb33e783c8304c1aadf0a339a9d
Ana Simplicio <asimplicio37@gmail.com>
2021-03-25 02:05:10 +0000

    Fase verde, testes passando

--- pratica_01/src/main/model/Calculadora.java
+++ pratica_01/src/main/model/Calculadora.java
@@ -3,7 +3,12 @@ package model;
 public class Calculadora {
 
     public int fatorial(int n) {
-        return 1;
-    }
+        int fat = 1;
+        for (int i = 1; i <= n; i = i + 1) {
+            fat = fat * i;
+        }
+
+        return fat;
+}
 
 }
