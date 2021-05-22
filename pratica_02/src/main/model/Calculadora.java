172834653cf286e47d178441b1b9772cf53813df
Ana Simplicio <asimplicio37@gmail.com>
2021-05-22 01:21:47 +0000

    Fase verde, testes passando

--- pratica_02/src/main/model/Calculadora.java
+++ pratica_02/src/main/model/Calculadora.java
@@ -5,6 +5,14 @@ public class Calculadora {
     public int fatorial(int n) {
         /* Realiza o cálculo do fatorial através do método recursivo. Para cada chamada de fatorial(n),
         o resultado será múltiplicado pelo valor de n anterior. */
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 0;
+        }
+
         return n * fatorial(n - 1);
     }
 
