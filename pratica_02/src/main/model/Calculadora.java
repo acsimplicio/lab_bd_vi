86a3f014554591c712f052aca71132bdcf381758
Ana Simplicio <asimplicio37@gmail.com>
2021-05-22 01:29:33 +0000

    Fase azul, refatoração

--- pratica_02/src/main/model/Calculadora.java
+++ pratica_02/src/main/model/Calculadora.java
@@ -5,15 +5,23 @@ public class Calculadora {
     public int fatorial(int n) {
         /* Realiza o cálculo do fatorial através do método recursivo. Para cada chamada de fatorial(n),
         o resultado será múltiplicado pelo valor de n anterior. */
-        if (n == 0) {
-            return 1;
-        }
-
-        if (n < 0) {
+        if (validarEntrada(n)) {
+            if (n == 0) {
+                return 1;
+            }
+            return n * fatorial(n - 1);
+        } else {
             return 0;
         }
+    }
 
-        return n * fatorial(n - 1);
+    private boolean validarEntrada(int n) {
+        /* Valida a entrada dada ao método fatorial, caso a entrada seja menor de 0, é inválida. */
+        if (n < 0) {
+            return false;
+        }
+
+        return true;
     }
 
 }
